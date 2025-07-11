899c138e856b8987edae71e66c0d5e06a2c94fb8
Onex <onexstha19@gmail.com>
2025-07-11 11:18:50 +0545

    first commit

---
+++ hjr.py
@@ -0,0 +1,7 @@
+echo "# samrat" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Onex-101/samrat.git
+git push -u origin main
